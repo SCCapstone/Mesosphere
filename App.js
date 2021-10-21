36a31e3c18ee3c762b8904aef462cdae98103be7
KTPrince <ktprince@email.sc.edu>
2021-10-20 20:27:14 -0400

    Moved styles into screen generator, where it should be.

--- App.js
+++ App.js
@@ -6,17 +6,7 @@ import ScreenGenerator from './ScreenGenerator';
 export default function App() {
   var Gen = new ScreenGenerator();
   Gen.generateScreen("Hey nobody change this line or we\'ll get a conflict! >:(");
-  //return;
-  return (
-    <View style={styles.container}><Text>{Gen.render()}</Text></View>
-  );
+  
+  return Gen.render();
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
