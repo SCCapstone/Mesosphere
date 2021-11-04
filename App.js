69af97a40711db46cf1aeffece8b767f8d5f7fa6
KTPrince <ktprince@email.sc.edu>
2021-11-04 11:12:20 -0400

    Updated files to match style guide (except ./__tests__)

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import ScreenGenerator from './ScreenGenerator'
 
 export default function App () {
--- ScreenGenerator.js
+++ ScreenGenerator.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
 export class ScreenGenerator extends Component {
-    output = "None";
-    constructor() {
-        super();
-    }
+  constructor () {
+    super()
+    // output = "None";
+  }
 
-    generateScreen(input) {
-        this.output = input;
-    }
+  generateScreen (input) {
+    this.output = input
+  }
 
-    render() {
-        return <View style={styles.container}><Text>{this.output}</Text></View>
-    }
+  render () {
+    return <View style={styles.container}><Text>{this.output}</Text></View>
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default ScreenGenerator
