d95aa885f3ece1a56b3dd20693c600c1fe13b25c
ZeHolyQofPower <adamstuff@outlook.com>
2022-02-10 13:26:17 -0500

    Removed blank media content in renderPost for instances of no media content.

--- Post.js
+++ Post.js
@@ -29,32 +29,46 @@ export class Post { // Post objects will be constructed from postPage() prompt
   decrementScore () {
     this.score -= 1
   }
-}
+}  // End of Post Class.
 
 // where it all comes together
 // will add const media, const score, const id as created
 
 export async function savePost (text, media) { // call with postText$.get() and postMedia$.get() as the two parameters
-  media = null // placeholder for eventual media content
   if (text.length > 50) {
     Alert.alert('Post too long', 'Posts can be, at most, 50 characers.', { text: 'OK.' })
   }
-  const p = new Post(generatePostID(), media, text, 0, new Date().toString())
-  const u = getUser()
-  u.addPost(p)
-  u.storeLocally()
+  // Post(postID, mediaContent, textContent, score, timestamp)
+  const new_post = new Post(generatePostID(), null, text, 0, new Date().toString())
+  const user = getUser()
+  user.addPost(new_post)
+  user.storeLocally()
   setScreen(PAGES.VIEWPOSTS)
 }
-
+// TODO(Gazdecki) Render vote buttons.
 export function renderPost (post) {
   const p = post.item
-  return (
-    <View style={styles.postContainer}>
-      <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
-      <Text style={styles.postContainerText}>{p.timestamp} </Text>
-      <Text style={styles.postContainerText}>Media content: {p.mediaContent} </Text>
-      <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
-      <Text style={styles.postContainerText}>Score: {p.score} </Text>
-    </View>
-  )
-} // TO BE FORMATTED
+  // If no media content, then don't render it lmao.
+  // TODO(Gazdecki) Compress this code.
+  if (p.mediaContent == null) {  //TODO(Gazdecki) Figure out how to comment inside of this return block correctly.
+    return (
+      <View style={styles.postContainer}>
+        <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
+        <Text style={styles.postContainerText}>{p.timestamp} </Text>
+        
+        <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
+        <Text style={styles.postContainerText}>Score: {p.score} </Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.postContainer}>
+        <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
+        <Text style={styles.postContainerText}>{p.timestamp} </Text>
+        <Text style={styles.postContainerText}>Media content: {p.mediaContent} </Text>
+        <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
+        <Text style={styles.postContainerText}>Score: {p.score} </Text>
+      </View>
+    )
+  }
+} // TO BE FORMATTED TODO(Gazdecki)
