77f31421b57f3bd6cbbd20fc8fcaee73b4423309
ZeHolyQofPower <adamstuff@outlook.com>
2022-02-11 10:38:58 -0500

    Added two vote buttons; Added async functions for interaction; Changed vote default to 1; Added console printing for button testing. Finished 1 TODO.

--- Post.js
+++ Post.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Alert } from 'react-native'
+import { View, Text, Alert, TouchableOpacity } from 'react-native'
 import { generatePostID, getUser, setScreen, styles, PAGES } from './Utility'
 
 // Tasks:
@@ -21,54 +21,65 @@ export class Post { // Post objects will be constructed from postPage() prompt
   getID () {
     return this.postID
   }
-
-  incrementScore () {
-    this.score += 1
-  }
-
-  decrementScore () {
-    this.score -= 1
-  }
 }  // End of Post Class.
+// TODO(Gazdecki) Consider compressing both vote buttons into a set of one function.
+// TODO(Gazdecki) Understand what the fuck you're doing here?
+// In order to live update, this function must be async?
+// It really should be inside of the post class.. I just can't seem to call anything in there?
+export async function incrementScore (post) {
+  // TODO(Gazdecki) Add check for if user has already voted here.
+  post.score += 1
+  console.log("Upvote button pressed, score is now: " + post.score)
+  // TODO(Gazdecki) Console says NaN currently, fix this.
+}
 
+export async function DecrementScore (post) {
+  // TODO(Gazdecki) Add check for if user has already voted here.
+  post.score -= 1
+  console.log("Downvote button pressed, score is now: " + post.score)
+}
 // where it all comes together
 // will add const media, const score, const id as created
-
 export async function savePost (text, media) { // call with postText$.get() and postMedia$.get() as the two parameters
   if (text.length > 50) {
     Alert.alert('Post too long', 'Posts can be, at most, 50 characers.', { text: 'OK.' })
   }
   // Post(postID, mediaContent, textContent, score, timestamp)
-  const new_post = new Post(generatePostID(), null, text, 0, new Date().toString())
+  const new_post = new Post(generatePostID(), null, text, 1, new Date().toString())
   const user = getUser()
   user.addPost(new_post)
   user.storeLocally()
   setScreen(PAGES.VIEWPOSTS)
 }
-// TODO(Gazdecki) Render vote buttons.
+
 export function renderPost (post) {
-  const p = post.item
-  // If no media content, then don't render it lmao.
-  // TODO(Gazdecki) Compress this code.
-  if (p.mediaContent == null) {  //TODO(Gazdecki) Figure out how to comment inside of this return block correctly.
-    return (
-      <View style={styles.postContainer}>
-        <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
-        <Text style={styles.postContainerText}>{p.timestamp} </Text>
-        
-        <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
-        <Text style={styles.postContainerText}>Score: {p.score} </Text>
-      </View>
-    )
-  } else {
-    return (
-      <View style={styles.postContainer}>
-        <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
-        <Text style={styles.postContainerText}>{p.timestamp} </Text>
-        <Text style={styles.postContainerText}>Media content: {p.mediaContent} </Text>
-        <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
-        <Text style={styles.postContainerText}>Score: {p.score} </Text>
-      </View>
-    )
-  }
+  const p = post.item  // Okay I'll fuckin' bite, what the hell is post.item?
+  return (
+    <View style={styles.postContainer}>
+      <Text style={styles.postContainerText}>Post ID: {p.postID} </Text>
+      <Text style={styles.postContainerText}>{p.timestamp} </Text>
+      {/**If no media content, then don't render it lmao. */}
+      {(p.mediaContent != null) && <Text style={styles.postContainerText}>Media content: {p.mediaContent} </Text>}
+      <Text style={styles.postContainerText}>Text content: {p.textContent} </Text>
+      <Text style={styles.postContainerText}>Score: {p.score} </Text>
+      {/**TODO(Gazdecki) Need to create a {styles.voteBtn} in the corrct place. Side-by-Side visually.*/}
+      {/**TODO(all) Best way to force 1-person:1-vote on each post? */}
+      {/**Upvote Button. */}
+      <TouchableOpacity
+        style={styles.postBtn} onPress={
+          () => { incrementScore(post) }
+        }
+      >
+        <Text style={styles.buttonText}> Upvote </Text>
+      </TouchableOpacity>
+      {/**Downvote Button. */}
+      <TouchableOpacity
+        style={styles.postBtn} onPress={
+          () => { decrementScore(post) }
+        }
+      >
+        <Text style={styles.buttonText}> Downvote </Text>
+      </TouchableOpacity>
+    </View>
+  )
 } // TO BE FORMATTED TODO(Gazdecki)
