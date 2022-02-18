458ec8ddeffb94955e07d4dba9e56a305e9e12d7
ZeHolyQofPower <adamstuff@outlook.com>
2022-02-18 14:46:12 -0500

    Made buttons work in console using a custom state hook; Removed 3 TODOs; Fixed firebase interaction call in both savePost and incrementScore; Still need to duplicate as decrementScore.

--- Post.js
+++ Post.js
@@ -7,27 +7,19 @@ export class Post extends React.Component {
   // Post objects will be constructed from postPage() prompt
   constructor (attachedMiD, postID, mediaContent, textContent, starting_score, timestamp) {
     super()
+    // TODO(Gazdecki) Supposedly we need this, but it doesn't work and I don't need it for suome reason?
+    // this.incrementScore = this.incrementScore.bind(this)
     this.attachedMiD = attachedMiD
     this.postID = postID
     this.mediaContent = mediaContent
     this.textContent = textContent
     // TODO(Gazdecki) Consider counting each type of vote seperately.
-    this.stateOfScore = {
-      score:starting_score
-    };
+    this.stateOfScore = { score:starting_score };
     this.timestamp = timestamp
   }
-  // TODO(Gazdecki) Determine if we need this? How does this relate to hooks?
-  //this.incrementScore=this.incrementScore.bind(this)
-  // TODO(Gazdecki) Examen the ScreenGenerator.js's use of a hook.
 
   getID () {
     return this.postID
-  }
-
-  decrementScore () {
-    this.score -= 1
-    alterPostScore(this.postID, -0.5)
   }
 
   getAttachedMID() {
@@ -37,12 +29,16 @@ export class Post extends React.Component {
 // TODO(Gazdecki) Consider compressing both buttons into a set of one function.
 
 // Must be an external function in order to be hooked correctly.
+// Must be async inorder to rerender.
 export async function incrementScore(post) {
-  // TODO(Gazdecki) Slowly ad these lines and get each one fully functioning.
-  post.stateOfScore={ score:69 }
-//  post.setStateOfScore({score : post.stateOfScore.score + 1})
-//  alterPostScore(post.postID, 0.5)
-  console.log("Upvote button pressed, score is now: ")// + post.StateOfScore:score )
+  post.stateOfScore = { score:(post.stateOfScore.score + 1) }
+  // TODO(Gazdecki) Why doesn't this work?:
+  /*
+  state = post.item.stateOfScore;
+  state = { score:(state.score + 1) }
+  */
+  console.log("Upvote button pressed, score is now: " + post.stateOfScore.score + ".")
+  alterPostScore(post.postID, 0.5)
 }
 
 // call with postText$.get() and postMedia$.get() as the two parameters
@@ -58,8 +54,7 @@ export async function savePost (text) {
                             new Date().toString())
   u.addPost(new_post)
   u.storeLocally()
-  // TODO(Gazdecki) Make this line work again, I believe I need to mod firebaseConfig.js
-  //pushPostToDatabase(new_post)
+  pushPostToDatabase(new_post)
   setScreen(PAGES.VIEWPOSTS)
 }
 
@@ -75,13 +70,13 @@ export function renderPost(post) {
         Media content: {p.mediaContent} </Text>}
       <Text style={styles.postContainerText}>
         Text content: {p.textContent} </Text>
-      {/**<Text style={styles.postContainerText}>
-        Score: {p.stateOfScore.score} </Text>*/}
+      <Text style={styles.postContainerText}>
+        Score: {p.stateOfScore.score} </Text>
       {/**TODO(Gazdecki) Need to pull @Carliegh 's button style she's got down in her beta. */}
       {/**TODO(all) Best way to force 1-person:1-vote on each post? */}
       {/**Upvote Button. */}
       <TouchableOpacity
-        style={styles.postBtn} onPress={ () => {incrementScore(post)} }
+        style={styles.postBtn} onPress={ () => {incrementScore(p)} }
       >
         <Text style={styles.buttonText}> Upvote </Text>
       </TouchableOpacity>
