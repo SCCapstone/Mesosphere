ec32df1439f8a3468eb857f4085648b793b4d303
KTPrince <ktprince@email.sc.edu>
2022-02-26 14:23:28 -0500

    Untested component-ized post.

--- Post.js
+++ Post.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, Alert, Button } from 'react-native'
 import { alterPostScore, pushPostToDatabase } from './firebaseConfig'
 import { generatePostID, getUser, setScreen, styles, PAGES,  } from './Utility'
 
-export class Post extends React.Component {
+export class Post extends Component{
   // Post objects will be constructed from postPage() prompt
   constructor (attachedMiD, postID, mediaContent, textContent, starting_score, interactedUsers, timestamp) {
-    super()
     // TODO(Gazdecki) Supposedly we need this, but it doesn't work and I don't need it for some reason?
     // this.incrementScore = this.incrementScore.bind(this)
     this.attachedMiD = attachedMiD
     this.postID = postID
     this.mediaContent = mediaContent
     this.textContent = textContent
-    this.stateOfScore = { score:starting_score };
+    this.starting_score = starting_score;
     this.interactedUsers = interactedUsers
     this.timestamp = timestamp
+    this.state = {
+      loading: false,
+      displayname: this.attachedMiD,
+      score: this.starting_score,
+      error: null,
+    }
   }
 
   getAttachedMID () {
@@ -27,25 +32,70 @@ export class Post extends React.Component {
   }
 
   incrementScore () {
-    this.score += 1
+    this.setState({ 
+      score: this.state.score + 1,
+    });
     alterPostScore(this.postID, 0.5)
   }
 
   decrementScore () {
-    this.score -= 1
+    this.setState({ 
+      score: this.state.score - 1,
+    });
     alterPostScore(this.postID, -0.5)
   }
 
   getInteractedUsers () {
     return this.interactedUsers
   }
-}  // End of Post Class.
-// Must be an external function in order to be hooked correctly.
-// Must be async inorder to rerender.
-export async function changeScore(post, change) {
-  post.stateOfScore = { score:(post.stateOfScore.score + change) }
-  console.log("Upvote button pressed, score is now: " + post.stateOfScore.score + ".")
-  alterPostScore(post.postID, 0.5)
+
+  componentDidMount () {
+    this.loadData()
+  }
+
+  async loadData () {
+    //Put fetching display name (and potentially score?) here.
+  }
+
+  async changeScore(change) {
+    this.setState({ 
+      score: this.state.score + change,
+    });
+    console.log("Upvote button pressed, score is now: " + post.state.score + ".")
+    alterPostScore(post.postID, 0.5)
+    //This should also add the current user to my interacted users.
+  }
+
+  render () {
+    return (
+      <View style={styles.postContainer}>
+        <Text style={styles.postContainerUsername}>{this.state.displayname} </Text>
+        <View style={{borderBottomColor: 'black',
+                      borderBottomWidth: 1,}}/>
+        <Text style={styles.postContainerText}>{this.textContent} </Text>
+        <Text style={styles.postContainerText}>{this.timestamp} </Text>
+        <View style={styles.scoreButtonStyle}>
+          <View style={styles.scoreButton}/>
+            <Button
+                onPress={() => {this.changeScore(1)}}
+                title="Like"
+                color="#254D32"
+                borderRadius='12'
+            />
+          <View style={styles.spacing}/>
+          <View style={styles.scoreButton}/>
+            <Button
+                onPress={() => {this.changeScore(-1)}}
+                title="Dislike"
+                color="#3A7D44"
+            />
+          <View style={styles.spacing}/>
+          <Text style={styles.postContainerText}>{this.state.score}</Text>
+          <View style={styles.spacing}/>        
+        </View>
+      </View>
+    )
+  }
 }
 
 export async function savePost (text) {
@@ -53,6 +103,7 @@ export async function savePost (text) {
     Alert.alert('Post too long',
                 'Posts can be, at most, 50 characers.',
                 { text: 'OK.' })
+                return;
   }
   
   const u = getUser()
@@ -96,7 +147,7 @@ export function renderPost (post) {
               color="#3A7D44"
           />
         <View style={styles.spacing}/>
-        <Text style={styles.postContainerText}>{p.stateOfScore.score}</Text>
+        <Text style={styles.postContainerText}>{p.score}</Text>
         <View style={styles.spacing}/>        
       </View>
     </View>
