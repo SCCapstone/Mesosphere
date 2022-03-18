094c1ac20d8ce796d1798698c537e2620faa071d
Carleigh Gregory <cjg3@email.sc.edu>
2022-03-18 14:39:00 -0400

    TextInput Fixes

--- Utility.js
+++ Utility.js
@@ -165,10 +165,12 @@ export const styles = StyleSheet.create({
     alignItems: 'center'
   },
   TextInput: {
+    width: "100%",
     height: 50,
     flex: 1,
     padding: 10,
-    marginLeft: 20
+    marginLeft: 20,
+    textAlign:'center'
   },
   loginBtn: {
     width: '80%',
